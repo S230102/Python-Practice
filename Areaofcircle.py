6868bca145a9d5c12fd58fc474e57173d7217606
S230102 <91896750+S230102@users.noreply.github.com>
2021-11-14 19:28:16 +0530

    Add files via upload

---
+++ Areaofcircle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 14:44:32 2021
+
+@author: Sharayu
+"""
+pi=3.14
+
+r = float(input("Input the radius of the circle:"))
+
+print("The area of the circle with radius "+str(r)+" is: "+str(pi*r**2))
